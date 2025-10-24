cd017628b17c821ba35d3c36874930c8847c0e0f
ArthurFernandes1 <arthur06lima1@gmail.com>
2025-10-23 22:45:20 -0300

    atualização 4

--- src/index.js
+++ src/index.js
@@ -24,37 +24,37 @@ let xp = 7600;
 let nivel;
 
 //verificação do nível do héroi | informa nome e nível 
-if(xp < 1000){
+if (xp < 1000) {
     nivel = Ferro;
-    console.log("O herói de nome" , nome," está no nível " , nivel)
+    console.log("O herói de nome", nome, " está no nível", nivel)
 }
 
-else if(xp > 1001 && xp < 2000){
+else if (xp > 1001 && xp < 2000) {
     nivel = "Bronze";
-    console.log("O herói de nome" , nome," está no nível " , nivel)
+    console.log("O herói de nome", nome, " está no nível", nivel)
 
-}else if(xp > 2001 && xp < 5000){
+} else if (xp > 2001 && xp < 5000) {
     nivel = "Prata";
-    console.log("O herói de nome" , nome," está no nível " , nivel)
+    console.log("O herói de nome", nome, " está no nível", nivel)
 
-}else if(xp > 6001 && xp < 7000 ){
+} else if (xp > 6001 && xp < 7000) {
     nivel = "Ouro";
-    console.log("O herói de nome" , nome," está no nível " , nivel)
+    console.log("O herói de nome", nome, " está no nível", nivel)
 
-}else if( xp > 7001 && xp < 8000){
+} else if (xp > 7001 && xp < 8000) {
     nivel = "Platina";
-    console.log("O herói de nome" , nome," está no nível ", nivel)
+    console.log("O herói de nome", nome, " está no nível", nivel)
 
-}else if( 8001 && xp < 9000){
+} else if (8001 && xp < 9000) {
     nivel = "Ascendente";
-    console.log("O herói de nome" , nome," está no nível ", nivel)
+    console.log("O herói de nome", nome, " está no nível", nivel)
 
-}else if(xp > 9001 && xp < 10000){
+} else if (xp > 9001 && xp < 10000) {
     nivel = "Imortal";
-    console.log("O herói de nome" , nome," está no nível ", nivel)
+    console.log("O herói de nome", nome, " está no nível", nivel)
 
-}else if(xp >= 10001){
+} else if (xp >= 10001) {
     nivel = "Radiante";
-    console.log("O herói de nome" , nome," está no nível ", nivel)
+    console.log("O herói de nome", nome, " está no nível", nivel)
 }
 
